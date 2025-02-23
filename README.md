a889079f72fe1c56ea463e5b2023c59a8152eb37
TheodorDM <st129282@studem.spbu.ru>
2025-02-23 21:15:40 +0300

    workpiace without YAML-file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LabWork2
+## Author
+
+Мызников Федор Денисович, группа 24.Б82-мм
+## Contacts
+
+st129282@student.spbu.ru
+## Description
---
+++ test.cpp
@@ -0,0 +1,12 @@
+//Myznikov Fedor Denisovich
+//st129282@student.spbu.ru
+//LabWork2
+
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
